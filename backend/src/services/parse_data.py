876b669c44bc79a8e4bad023d24cca7be2e6912c
ParkieV <st-psk@mail.ru>
2024-11-27 01:52:08 +0300

    Add parsing for docs and txt

--- backend/src/services/parse_data.py
+++ backend/src/services/parse_data.py
@@ -1,26 +1,33 @@
+from io import BytesIO
 from typing import Any
 
 import fitz
+from docx import Document
 
 from fastapi import UploadFile
 
 
 async def parse_files(files: list[UploadFile]):
     for file in files:
-        if not file.filename.endswith('.pdf'):
-            raise ValueError('Could not parse file')
+        match filename := file.filename:
+            case filename.endswith('.pdf'):
+                await _parse_pdf(file)
+            case filename.endswith('.docx'):
+                ...
+            case filename.endswith('.txt'):
+                ...
+            case _:
+                raise ValueError('Could not parse file')
 
-        await parse_pdf(file)
 
-async def parse_pdf(file: UploadFile) -> list[dict[str, Any]]:
+async def _parse_pdf(file: UploadFile) -> list[dict[str, Any]]:
     pdf = fitz.open(stream=await file.read(), filetype='pdf')
-    all_info_about_pdf = []
+    file_info = []
     for number_of_page, page in enumerate(pdf):
         text = page.get_text().replace('\n', ' ').strip()
 
-        all_info_about_pdf.append({
+        file_info.append({
             'metadata': {
-                "page_number": number_of_page + 1,  # номер страницы
                 "page_char_counts": len(text),  # количество символов
                 "page_word_counts": len(text.split(" ")),  # количество слов
                 "page_sents_counts": len(text.split(". ")),  # количество предложений
@@ -28,4 +35,45 @@ async def parse_pdf(file: UploadFile) -> list[dict[str, Any]]:
             },
             "text": text  # текст
         })
-    return all_info_about_pdf
+    return file_info
+
+async def _parse_txt(file: UploadFile) -> list[dict[str, Any]]:
+    text = (await file.read()).decode('utf-8').replace('\n', ' ').strip()
+
+    file_info = {
+        'metadata': {
+            "page_number": 1,  # номер страницы
+            "page_char_counts": len(text),  # количество символов
+            "page_word_counts": len(text.split(" ")),  # количество слов
+            "page_sents_counts": len(text.split(". ")),  # количество предложений
+            "page_token_counts(approximately)": len(text) / 4,  # количество токенов
+        },
+        "text": text  # текст
+    }
+    return [file_info]
+
+async def _parse_docx(file: UploadFile) -> list[dict[str, Any]]:
+    content = await file.read()
+
+    # Используем BytesIO для работы с данными в памяти
+    doc = Document(BytesIO(content))
+
+    # Извлекаем текст из документа
+    text = []
+    for paragraph in doc.paragraphs:
+        text.append(paragraph.text)
+
+    # Объединяем текст в один блок
+    full_text = " ".join(text)
+
+    file_info = {
+        'metadata': {
+            "paragraph_char_counts": len(full_text),  # количество символов
+            "paragraph_word_counts": len(full_text.split(" ")),  # количество слов
+            "paragraph_sents_counts": len(full_text.split(". ")),  # количество предложений
+            "paragraph_token_counts(approximately)": len(full_text) / 4,  # количество токенов
+        },
+        "text": full_text  # текст
+    }
+
+    return [file_info]
