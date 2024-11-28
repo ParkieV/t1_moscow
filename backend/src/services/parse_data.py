7aeb6da71b4a3c805b219444654904d457cbe1c5
ParkieV <st-psk@mail.ru>
2024-11-28 11:00:55 +0300

    Fix

--- backend/src/services/parse_data.py
+++ backend/src/services/parse_data.py
@@ -2,7 +2,9 @@ from io import BytesIO
 from typing import Any
 from uuid import UUID
 
+import aiohttp
 import fitz
+from bs4 import BeautifulSoup
 from docx import Document
 
 from fastapi import UploadFile
@@ -10,8 +12,20 @@ from fastapi import UploadFile
 from src.repositories.postgres import PostgresContext
 from src.repositories.postgres.data import FileCRUD
 
+async def is_url(url: str) -> bool:
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    return True
 
-async def parse_files(files: list[UploadFile], assistant_id: UUID):
+                return False
+    except Exception as e:
+        return False
+
+async def parse_files(files: list[UploadFile],
+                      urls: list[str],
+                      assistant_id: UUID):
     db_context = PostgresContext[FileCRUD](crud=FileCRUD(session_factory=PostgresContext.new_session))
     res = []
     for file in files:
@@ -27,8 +41,22 @@ async def parse_files(files: list[UploadFile], assistant_id: UUID):
             data_info = await _parse_txt(file)
             await db_context.crud.insert_file(assistant_id, data_info)
             res.append(data_info)
+        elif file.filename.endswith('.html'):
+            content = await file.read()
+            soup = BeautifulSoup(content, 'html.parser')
+            page_text = soup.get_text()
+            data_info = await _parse_url(page_text)
+            await db_context.crud.insert_file(assistant_id, data_info)
+            res.append(data_info)
+
         else:
             raise ValueError('Could not parse file')
+    for url in urls:
+        if await is_url(url):
+            data_info = await _parse_url(await _get_page_data(url))
+            await db_context.crud.insert_file(assistant_id, data_info)
+            res.append(data_info)
+
 
     return res
 
@@ -90,3 +118,30 @@ async def _parse_docx(file: UploadFile) -> list[dict[str, Any]]:
     }
 
     return [file_info]
+
+async def _parse_url(page_text: str | None):
+    if page_text is not None:
+        text = page_text.replace('\n', ' ').strip()
+        file_info = {
+            'metadata': {
+                "char_counts": len(text),  # количество символов
+                "word_counts": len(text.split(" ")),  # количество слов
+                "sents_counts": len(text.split(". ")),  # количество предложений
+                "token_counts(approximately)": len(text) / 4,  # количество токенов
+            },
+            "text": text  # текст
+        }
+        return [file_info]
+    else:
+        raise ValueError('Could not parse page')
+
+async def _get_page_data(url: str) -> str | None:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                content = await response.text()
+                soup = BeautifulSoup(content, 'html.parser')
+                return soup.get_text()
+            else:
+                print(f"Error: {response.status}")
+                return None
